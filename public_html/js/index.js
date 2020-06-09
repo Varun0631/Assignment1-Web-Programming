03d09ca38151ded49c6c17c18e11c440e5e04d7d
Varun <Varun@Varun>
2020-06-08 22:39:56 -0400

    This is final half of javascript file

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -20,3 +20,32 @@ function nextques(){
 		document.getElementById('score').style.display = "block";
 	}
 }
+
+function check(){
+	
+	document.getElementById(ques[pos][0]).style.display = "none";
+	if(document.getElementById(ques[pos][1]).checked){
+            
+		document.getElementById("correct").style.display = "block";
+		score++;
+		console.log(score);
+                
+	}else{
+		document.getElementById("wrong").style.display = "block";
+	}
+	pos ++;
+}
+
+function restart(){
+    
+	score = 0;
+	pos =0;
+	document.getElementById("dv3").style.display = "none";
+	document.getElementById("correct").style.display = "none";
+	document.getElementById("wrong").style.display = "none";
+	document.getElementById("score").style.display = "none";
+	
+	document.getElementById(ques[0][0]).style.display = "block";
+	
+}
+
